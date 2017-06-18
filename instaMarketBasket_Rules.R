f9e08b38c6cc5ef2f944f28aae679a14ab345ce7
Jansi Thiyagarajan <thiyagarajan.jansi@gmail.com>
2017-06-19 00:40:50 +0530

    Target 47766 Included

--- instaMarketBasket_Rules.R
+++ instaMarketBasket_Rules.R
@@ -23,7 +23,28 @@ frequentItems_top10 =  itemFrequencyPlot(instaTransactions, topN = 10)
 
 # rules mining 
 instaRules_sup5 = apriori(instaTransactions, parameter=list(support=0.05, confidence=0.8, maxlen = 4))
-instaRules_sup1 = apriori(instaTransactions, parameter=list(support=0.01, confidence=0.8, maxlen = 4))
+instaRules_sup01 = apriori(instaTransactions, parameter=list(support=0.0001, confidence=0.8))
 instaRules_sup10 = apriori(instaTransactions, parameter=list(support=0.1, confidence=0.8, maxlen = 2))
 
-inspect(instaRules_sup10)
+inspect(instaRules_sup01)
+
+# Plot network graph for items 
+plot(instaRules_sup01,method="graph",interactive=TRUE,shading=NA)
+
+instaRules_sup01<-sort(instaRules_sup01, by="confidence", decreasing=TRUE)
+
+# Write the rules to csv 
+write(instaRules_sup01, "instaSup001Rules.csv", sep=",") 
+
+# Rules with target item 
+
+target47766 <-apriori(instaTransactions, parameter=list(supp=0.001,conf = 0.08), 
+               appearance = list(default="lhs",rhs="47766"),
+               control = list(verbose=F))
+
+target47766 <- sort(target47766, decreasing=TRUE,by="confidence")
+
+# Show the top 5 rows 
+inspect(target47766[1:5])
+
+
